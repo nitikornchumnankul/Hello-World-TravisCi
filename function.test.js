adf6756d50ae14acae2e5229b702f346cc0584ec
Nitikorn Chumnankul <nitikornchumnankul@gmail.com>
2019-08-12 00:27:22 +0700

    comment mistake

--- function.test.js
+++ function.test.js
@@ -3,8 +3,8 @@ test("I am computer engineering",()=>{
     expect("hello world").not.toMatch(/I/);
 });
 
-test('User fetched name should be Leanne Graham', async () =>{
-    expect.assertion(1);
-    const data = await functions.fetchUser();
-    expect(data.name).toEqual('Leanne Graham');
-});
+// test('User fetched name should be Leanne Graham', async () =>{
+//     expect.assertion(1);
+//     const data = await functions.fetchUser();
+//     expect(data.name).toEqual('Leanne Graham');
+// });
