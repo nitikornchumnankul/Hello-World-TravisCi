80971731657d6976882f8497ef9c1d75f79aa57f
nitikornchumnankul <nitikornchumnankul@gmail.com>
2020-02-08 15:57:02 +0700

    test create image from docker hub

--- README.md
+++
@@ -1,2 +0,0 @@
-[![Build Status](https://travis-ci.org/nitikornchumnankul/Hello-World-CircleCi.svg?branch=master)](https://travis-ci.org/nitikornchumnankul/Hello-World-CircleCi)
-# Hello-World-TravisCi
--- function.test.js
+++
@@ -1,7 +0,0 @@
-const functions = require('./function');
-
-test('User fetched name should be Leanne Graham', async () => {
-  expect.assertions(1);
-  const data = await functions.fetchUser();
-  expect(data.name).toEqual('Leanne Graham');
-});
